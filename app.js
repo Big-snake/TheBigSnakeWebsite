c56806fd181abbc538ed5c948f8c020b4a888e3b
Big-snake <52288545+Big-snake@users.noreply.github.com>
2020-01-25 20:56:10 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function onload() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./luft.mp4");
+    xhr.responseType = "arraybuffer";
+ 
+    xhr.onload = function(error) {
+        var blob = new Blob([xhr.response]);
+        var blobUrl = URL.createObjectURL(blob);
+        console.log(blobUrl);
+        document.getElementById("blob").src = blobUrl;
+    }
+    xhr.send();
+}
